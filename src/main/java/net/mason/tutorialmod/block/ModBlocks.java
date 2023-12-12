772eb13ba24335ef20568cabaf4586d123739452
Mason <masonritchotte@gmail.com>
2023-12-12 14:56:35 -0500

    Sound Block - Advanced Item

--- src/main/java/net/mason/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/mason/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.mason.tutorialmod.block;
 
 import net.mason.tutorialmod.TutorialMod;
+import net.mason.tutorialmod.block.custom.SoundBlock;
 import net.mason.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
@@ -39,6 +40,9 @@ public class ModBlocks {
     public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)
                     .strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
+    public static final RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block",
+            () -> new SoundBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
